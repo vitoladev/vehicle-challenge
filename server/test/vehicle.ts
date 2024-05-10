467324906199ccae53ec2ebe5cc0b25051c07583
vitoladev <asaptorvi@protonmail.com>
2024-05-10 01:05:10 -0300

    test: vehicle testing utils

---
+++ server/test/vehicle.ts
@@ -0,0 +1,22 @@
+import { faker } from "@faker-js/faker";
+import app from "../src/app";
+import { vehicles } from "../src/common/db/schema";
+
+export const vehiclePlateMock = () =>
+  `${faker.string.alpha({ length: 3 }).toUpperCase()}-${faker.number.int({
+    min: 1000,
+    max: 9999,
+  })}`;
+
+export const vehicleMock = () => ({
+  placa: vehiclePlateMock(),
+  chassi: faker.vehicle.vin(),
+  renavam: faker.number.int({ min: 100000000, max: 99999999999 }),
+  modelo: faker.vehicle.model(),
+  marca: faker.vehicle.manufacturer(),
+  ano: faker.number.int({ min: 1886, max: 2024 }),
+});
+
+export const cleanUpVehicles = async () => {
+  await app.db.delete(vehicles);
+};
