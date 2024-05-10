f0484b7e806427be217d93c81bbccddd158f7bb8
vitoladev <asaptorvi@protonmail.com>
2024-05-10 01:05:55 -0300

    feat: add unique indexes for placa, chassi, and renavam in vehicles table

---
+++ server/src/common/db/migrations/0001_short_zeigeist.sql
@@ -0,0 +1,3 @@
+CREATE UNIQUE INDEX IF NOT EXISTS "placa_idx" ON "vehicles" ("placa");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "chassi_idx" ON "vehicles" ("chassi");--> statement-breakpoint
+CREATE UNIQUE INDEX IF NOT EXISTS "renavam_idx" ON "vehicles" ("renavam");
--- server/src/common/db/schema.ts
+++ server/src/common/db/schema.ts
@@ -4,15 +4,26 @@ import {
   pgTable,
   serial,
   text,
+  uniqueIndex,
   varchar,
 } from "drizzle-orm/pg-core";
 
-export const vehicles = pgTable("vehicles", {
-  id: serial("id").primaryKey(),
-  placa: text("placa").unique().notNull(),
-  chassi: varchar("chassi", { length: 17 }).unique().notNull(),
-  renavam: bigint("renavam", { mode: "number" }).unique().notNull(),
-  modelo: text("modelo").notNull(),
-  marca: text("marca").notNull(),
-  ano: integer("ano").notNull(),
-});
+export const vehicles = pgTable(
+  "vehicles",
+  {
+    id: serial("id").primaryKey(),
+    placa: text("placa").unique().notNull(),
+    chassi: varchar("chassi", { length: 17 }).unique().notNull(),
+    renavam: bigint("renavam", { mode: "number" }).unique().notNull(),
+    modelo: text("modelo").notNull(),
+    marca: text("marca").notNull(),
+    ano: integer("ano").notNull(),
+  },
+  (table) => {
+    return {
+      placaIdx: uniqueIndex("placa_idx").on(table.placa),
+      chassiIdx: uniqueIndex("chassi_idx").on(table.chassi),
+      renavamIdx: uniqueIndex("renavam_idx").on(table.renavam),
+    };
+  }
+);
