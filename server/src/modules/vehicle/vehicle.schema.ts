60e31e1dfd289cb97a947d1f58ad1647ab07d77c
vitoladev <asaptorvi@protonmail.com>
2024-05-10 01:06:36 -0300

    feat: Add validation for placa field in vehicle schema

--- server/src/modules/vehicle/vehicle.schema.ts
+++ server/src/modules/vehicle/vehicle.schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const vehicleSchema = z.object({
-  placa: z.string(),
+  placa: z.string().refine((placa) => /^[A-Z]{3}-[0-9]{4}$/.test(placa)),
   chassi: z.string().length(17),
   renavam: z.number().int().min(100000000).max(99999999999),
   modelo: z.string(),
@@ -10,3 +10,10 @@ export const vehicleSchema = z.object({
 });
 
 export type VehicleSchema = z.infer<typeof vehicleSchema>;
+
+export const vehicleIdParamSchema = z.object({
+  id: z
+    .string()
+    .refine((id) => Number.isInteger(Number(id)) && Number(id) > 0)
+    .transform((id) => Number(id)),
+});
